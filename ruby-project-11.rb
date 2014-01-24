9d6bd1cffdfc7deb58350c68b024f5edd19e99f3
Aina Tahiry <utouch25@gmail.com>
2014-01-24 12:18:49 +0300

    update, ruby-project-11.rb

--- ruby-project-11.rb
+++ ruby-project-11.rb
@@ -1,31 +1,30 @@
 class Account
  attr_accessor :account_holder
  def initialize
- 	@balance = 0
- 	@journal = []
+   @balance = 0
+   @journal = []
  end
 
  def balance
- 	return @balance
+   return @balance
  end
 
  def deposit(amount)
- 	@balance += amount
- 	@journal << ["Deposited: #{amount}"]
- 	return @balance
+   @balance += amount
+   @journal << ["Deposited: #{amount}"]
+   return @balance
  end
 
  def withdraw(amount)
- 	@balance -= amount
- 	@journal << ["Withdrew: #{amount}"]
- 	return @balance
+   @balance -= amount
+   @journal << ["Withdrew: #{amount}"]
+   return @balance
  end
 
  def transfer(amount, account2)
- 	account2.deposit(amount)
- 	withdraw(amount)
+   account2.deposit(amount)
+   withdraw(amount)
  end
 end
 
-
- 
+# Time spent = 3 hours
